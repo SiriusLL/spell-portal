dda1c60ef2bae8f6134810abfab4548dfbe10940
SiriusLL <sirius.love.light@gmail.com>
2022-03-04 02:51:02 -0700

    card styling

--- src/App.js
+++ src/App.js
@@ -1,7 +1,29 @@
-import "./App.css";
+import React from "react";
+import { ethers } from "ethers";
+import './App.css';
 
-function App() {
-  return <div className="App"></div>;
+export default function App() {
+
+  const portal = () => {
+    
+  }
+  
+  return (
+    <div className="mainContainer">
+
+      <div className="dataContainer">
+        <div className="header">
+        🧙‍♂️ Kablammy 🌬️🌒💫✨⚡️
+        </div>
+
+        <div className="bio">
+        I am a Wizard who travels the Zuvuya.  Connect your Ethereum wallet and open 1 or more portals to send a spell of manifestation and you might receive a gift through the portal.
+        </div>
+
+        <button className="portalButton" onClick={portal}>
+          Cast a spell
+        </button>
+      </div>
+    </div>
+  );
 }
-
-export default App;
